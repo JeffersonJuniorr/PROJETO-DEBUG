0b2354aba06170bf48ad413d832da9fa576a0aae
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-24 09:30:25 -0300

    Removendo linhas repetidas e

--- js/login.js
+++ js/login.js
@@ -9,26 +9,24 @@ document.addEventListener('click', function (event) {
     if (!home.contains(event.target) && event.target !== formOpenBtn) {
         home.classList.remove("show");
     }
-});
+})
   
 const container = document.querySelector(".container");
 const sign_in_btn = document.querySelector("#sign-in-btn");
-const sign_up_btn = document.querySelector("#sign-up-btn");
-const sign_in_btn2 = document.querySelector("#sign-in-btn2");
 const sign_up_btn2 = document.querySelector("#sign-up-btn2");
 
 sign_up_btn.addEventListener("click", () => {
     container.classList.add("sign-up-mode");
-});
+})
 
 sign_in_btn.addEventListener("click", () => {
     container.classList.remove("sign-up-mode");
-});
+})
 
 sign_up_btn2.addEventListener("click", () => {
     container.classList.add("sign-up-mode2");
-});
+})
 sign_in_btn2.addEventListener("click", () => {
     container.classList.remove("sign-up-mode2");
-});
+})
 //efito login
